5477375044b3cbd01861da728842ea7e9c975ad2
Roman Gadjak <romangadjak@gmail.com>
2025-11-06 20:23:31 +0200

    refac: RegisterRequest displayName requirement tweak

--- backend/src/main/kotlin/com/dotcom/retail/domain/auth/dto/RegisterRequest.kt
+++ backend/src/main/kotlin/com/dotcom/retail/domain/auth/dto/RegisterRequest.kt
@@ -10,7 +10,7 @@ data class RegisterRequest(
     @field:NotBlank(message = "Password must not be empty")
     val password: String,
 
-    @field:NotBlank(message = "First name must not be empty")
-    @field:Size(min = 1, max = 16, message = "Name must be between 1 and 16 characters.")
+    @field:NotBlank(message = "Name must not be empty")
+    @field:Size(min = 1, max = 16, message = "Name must be between 1 and 30 characters.")
     val displayName: String,
 )
