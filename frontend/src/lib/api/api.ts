64f7951703b666462100605965a13c54e24836b1
Roman Gadjak <romangadjak@gmail.com>
2025-11-17 20:39:45 +0200

    feat(front): api logging

--- frontend/src/lib/api/api.ts
+++ frontend/src/lib/api/api.ts
@@ -1,6 +1,7 @@
 import axios, {InternalAxiosRequestConfig} from "axios";
 import {tokenManager} from "@lib/auth/tokenManager";
 import {AUTHORIZATION_HEADER_BEARER} from "@constants/auth";
+import { logger as log } from "@lib/logger";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -13,16 +14,37 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use(
-    (config: InternalAxiosRequestConfig) => {
-        const token = tokenManager.getAccessToken();
-        if (token) {
-            config.headers.Authorization = `${AUTHORIZATION_HEADER_BEARER} ${token}`;
-        }
+    config => {
+        log.d("[API]", {
+            method: config?.method?.toUpperCase(),
+            url: config.baseURL
+        });
+
+        setBearerToken(config)
         return config;
     },
-    (error) => Promise.reject(error)
+
+    error => Promise.reject(error)
 );
 
+api.interceptors.response.use(
+    response => {
+        // log.api()
+        return response;
+    },
 
+    error => {
+        log.api(error.response.config.method, error.response.config.baseURL, error.status)
+        return Promise.reject(error);
+    }
+)
+
+function setBearerToken(config: InternalAxiosRequestConfig) {
+    const token = tokenManager.getAccessToken();
+    if (token) {
+        config.headers.Authorization = `${AUTHORIZATION_HEADER_BEARER} ${token}`;
+    }
+    return config;
+}
 
 export default api;
