914332db6b3ce2c134cdf991bfd7b73745a25a18
Roman Gadjak <romangadjak@gmail.com>
2026-03-12 20:00:13 +0200

    rm: Transaction entity

--- backend/src/main/kotlin/com/dotcom/retail/domain/payment/TransactionRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.dotcom.retail.domain.payment
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface TransactionRepository : JpaRepository<Transaction, UUID> {
-    fun findByExternalId(externalId: String): Transaction?
-}
