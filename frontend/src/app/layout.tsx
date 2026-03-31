bebf24dc0fb9de29636eff899b13e330082b80ab
Roman Gadjak <romangadjak@gmail.com>
2026-03-31 20:13:06 +0300

    fix[frontend]: Remove global color transition (light & dark mode) on layout. New default font size 16px

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,38 +1,35 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import React from "react";
-import {AuthProvider} from "@lib/auth/authContext";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
-import {ThemeProvider} from "next-themes";
-import Toast from "@components/Toast";
-import ProtectedRoute from "@components/ProtectedRoute";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import React from "react"
+import { AuthProvider } from "@lib/auth/authContext"
+import { ThemeProvider } from "next-themes"
+import Toast from "@components/Toast"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "dot-com-retail",
   description: "",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased transition-colors`}
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <AuthProvider>
@@ -42,5 +39,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
